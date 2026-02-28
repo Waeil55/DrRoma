6586506c71e40f829b418dfd7bba66b99011991e
Waeil Mikhaeil <102403413+Waeil55@users.noreply.github.com>
2026-02-27 23:53:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is mandatory for GitHub Pages subdirectories, otherwise it results in a blank white page!
-  base: '/DrMariam/', 
+  base: '/DrMariam/',
 })
 
 
