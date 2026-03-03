52685935300947f1bfbb5e83e2e495d26aa57f49
Waeil55 <waeil.usa@gmail.com>
2026-03-03 04:57:15 -0500

    Physically removed conflict markers and synced config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'DrMariam' with your exact repository name if different
   base: '/DrMariam/', 
 })
